28d70c8f34a311f1171ab2c8107b19da905f7cdc
varun <blazerrryt@gmail.com>
2024-07-06 22:44:05 -0700

    test

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -6,11 +6,11 @@ Read more: https://developer.mozilla.org/en-US/docs/Web/Security/Secure_Contexts
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"bb10c5466638e963479ba5e64e601e42d1a43447","builds":[{"compileTarget":"dart2js","renderer":"html","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"bb10c5466638e963479ba5e64e601e42d1a43447","builds":[{"compileTarget":"dart2js","renderer":"auto","mainJsPath":"main.dart.js"}]};
 
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3240902629"
+    serviceWorkerVersion: "4259025382"
   }
 });
